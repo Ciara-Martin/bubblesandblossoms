160d7fe3de25ea1c43f6b075c3e540c5804d277e
Ciara Martin <ciaramartinn18@gmail.com>
2024-12-04 13:40:58 +0000

    Initial commit

---
+++ src/main/kotlin/models/Dog.kt
@@ -0,0 +1,8 @@
+package ie.setu.models
+
+data class Breed(var breedId: Int = 0,
+                 var nameofbreed: String,
+                 var colour: String,
+                 var size: String,
+                 var coattype: String,
+                 var price: Int)
