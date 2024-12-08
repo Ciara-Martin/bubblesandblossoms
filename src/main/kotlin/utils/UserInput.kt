9686036a73bd9360edf23504361d3adc447a31d6
Ciara Martin <ciaramartinn18@gmail.com>
2024-12-08 20:29:15 +0000

    Final Commit

--- src/main/kotlin/utils/UserInput.kt
+++ src/main/kotlin/utils/UserInput.kt
@@ -1,6 +1,5 @@
 package utils
 
-fun readIntNotNull() = readlnOrNull()?.toIntOrNull() ?: -1
 
 /**
  * Reads an integer from the user input after displaying a prompt.
@@ -20,31 +19,6 @@ fun readNextInt(prompt: String?): Int {
     } while (true)
 }
 
-fun readDoubleNotNull() = readlnOrNull()?.toDoubleOrNull() ?: -1.0
-
-fun readNextDouble(prompt: String?): Double {
-    do {
-        try {
-            print(prompt)
-            return readln().toDouble()
-        } catch (e: NumberFormatException) {
-            System.err.println("\tEnter a decimal number please.")
-        }
-    } while (true)
-}
-
-fun readFloatNotNull() = readlnOrNull()?.toFloatOrNull() ?: -1.0f
-
-fun readNextFloat(prompt: String?): Float {
-    do {
-        try {
-            print(prompt)
-            return readln().toFloat()
-        } catch (e: NumberFormatException) {
-            System.err.println("\tEnter a decimal number please.")
-        }
-    } while (true)
-}
 
 /**
  * Reads a string from the user input after displaying a prompt.
@@ -55,16 +29,4 @@ fun readNextFloat(prompt: String?): Float {
 fun readNextLine(prompt: String?): String {
     print(prompt)
     return readln()
-}
-
-
-fun readNextChar(prompt: String?): Char {
-    do {
-        try {
-            print(prompt)
-            return readln().first()
-        } catch (e: Exception) {
-            System.err.println("\tEnter a character please.")
-        }
-    } while (true)
 }
